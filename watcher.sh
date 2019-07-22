9a93b473d54c91bd3f3721ca5f85a4bef9df1ca5
Thanh Tu <tu@novalex.vn>
2019-07-22 10:28:40 +0700

    update starter kit

--- watcher.sh
+++ watcher.sh
@@ -6,13 +6,13 @@ CMD=$BASE_DIR/bin
 case "${unameOut}" in
     Linux*)     CMD="$CMD/Linux/air";;
     Darwin*)    CMD="$CMD/Mac/air";;
-    CYGWIN*|MINGW*)    CMD="$CMD/Win/air.exe";;    
-    *)          
-      echo "UNKNOWN:${unameOut}"
-      exit 1
+    CYGWIN*|MINGW*)    CMD="$CMD/Win/air.exe";;
+    *)
+        echo "UNKNOWN:${unameOut}"
+        exit 1
     ;;
 esac
 
 COMMAND=${1:-api}
-cd backend
+cd ../backend
 $CMD -c conf/air/${COMMAND}.conf
